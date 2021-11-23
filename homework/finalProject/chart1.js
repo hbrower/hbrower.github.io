c75aa07af67468fcae7e97682533ec0fbf445e99
hbrower <h.brower@northeastern.edu>
2021-11-22 22:29:03 -0500

    hw session 2 11.22

--- homework/finalProject/chart1.js
+++ homework/finalProject/chart1.js
@@ -1,12 +1,14 @@
 d3.select("#chart1")
   .on("mousemove", function() {
+
     var tooltip = d3.select("#tooltip")
       .style("display", "block")
       .style("top", d3.event.pageY + 20 + "px")
-      .style("left", d3.event.pageX + 20 + "px")
-    
+      .style("left", d3.event.pageX + 20 + "px");
+
     tooltip.select("#title").html("State");
-    tooltip.select("#value").html("Arrest");
+    tooltip.select("#value").html("Arrests");
+
   })
   .on("mouseout", function() {
     d3.select("#tooltip")
