c130ea7fa1e72e2244a3440bc581a26bda27b946
hbrower <h.brower@northeastern.edu>
2021-11-29 21:38:17 -0500

    hw session 11.29

--- homework/finalProject/chart1.js
+++ homework/finalProject/chart1.js
@@ -1,3 +1,4 @@
+//Tooltip
 d3.select("#chart1")
   .on("mousemove", function() {
 
@@ -14,3 +15,60 @@ d3.select("#chart1")
     d3.select("#tooltip")
       .style("display", "none");
   });
+
+//Treemap Chart
+
+    // set the dimensions and margins of the graph
+    var margin = {top: 0, right: 0, bottom: 0, left: 0},
+      width = 1400 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+    
+    // append the svg object to the body of the page
+    var svg = d3.select("#chart1")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+    
+    // Read data
+    d3.csv('state_data.csv', function(data) {
+    
+    var root = d3.stratify()
+    .id(function(d) { return d.state; })   
+    .parentId(function(d) { return d.parent; })   
+    (data);
+  root.sum(function(d) { return +d.arrests })   
+      d3.treemap()
+        .size([width, height])
+        .padding(4)
+        (root)
+    
+    console.log(root.leaves())
+      // use this information to add rectangles:
+
+      svg
+        .selectAll("rect")
+        .data(root.leaves())
+        .enter()
+        .append("rect")
+          .attr('x', function (d) { return d.x0; })
+          .attr('y', function (d) { return d.y0; })
+          .attr('width', function (d) { return d.x1 - d.x0; })
+          .attr('height', function (d) { return d.y1 - d.y0; })
+          .style("stroke", "black")
+          .style("fill", "#69b3a2");
+    
+      // and to add the text labels
+      svg
+        .selectAll("text")
+        .data(root.leaves())
+        .enter()
+        .append("text")
+          .attr("x", function(d){ return d.x0+10})    // +10 to adjust position (more right)
+          .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
+          .text(function(d){ return d.data.state})
+          .attr("font-size", "10px")
+          .attr("fill", "black")
+    })
